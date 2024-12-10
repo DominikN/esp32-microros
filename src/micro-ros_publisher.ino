58ce8b478966972ff763527aa9e61f947abccd7d
Dominik Nowak <dominik.nwk@gmail.com>
2024-12-10 19:57:27 +0100

    publishing from a separate task instead of timer is better (no loop delay) and still threadsafe. However you need to block the publishing when entities are removed

--- src/micro-ros_publisher.ino
+++ src/micro-ros_publisher.ino
@@ -38,6 +38,8 @@ std_msgs__msg__String msg_out;
 
 char buffer_out[500];
 
+TickType_t xLastHeartbeatTime;
+
 #define AGENT_PORT 8888
 #define NODE_NAME "talker_esp32"
 #define ROS_NAMESPACE ""
@@ -49,33 +51,43 @@ char buffer_out[500];
 #define LED_PIN 13
 #endif
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+#define RCCHECK(fn)              \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+      return false;              \
+    }                            \
+  }
+#define RCSOFTCHECK(fn)          \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+    }                            \
+  }
 
-enum states
+void publishHelloWorld()
 {
-  WAITING_AGENT,
-  AGENT_AVAILABLE,
-  AGENT_CONNECTED,
-  AGENT_DISCONNECTED
-} state;
+  static int cnt = 0;
+
+  sprintf(buffer_out, "Hello World: %d", cnt++);
+  // Serial.printf("Publishing: \"%s\" [free heap: %d]\r\n", buffer,
+  //              xPortGetFreeHeapSize());
+
+  msg_out.data = micro_ros_string_utilities_set(msg_out.data, buffer_out);
+
+  RCSOFTCHECK(rcl_publish(&publisher, &msg_out, NULL));
+
+  micro_ros_string_utilities_destroy(&(msg_out.data));
+}
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL)
   {
-    static int cnt = 0;
-
-    sprintf(buffer_out, "Hello World: %d", cnt++);
-    // Serial.printf("Publishing: \"%s\" [free heap: %d]\r\n", buffer,
-    //              xPortGetFreeHeapSize());
-
-    msg_out.data = micro_ros_string_utilities_set(msg_out.data, buffer_out);
-
-    RCSOFTCHECK(rcl_publish(&publisher, &msg_out, NULL));
-
-    micro_ros_string_utilities_destroy(&(msg_out.data));
+    publishHelloWorld();
   }
 }
 
@@ -85,6 +97,7 @@ void subscription_callback(const void *msgin)
   Serial.printf("I heard: [%s]\r\n", msg->data.data);
 
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  xLastHeartbeatTime = xTaskGetTickCount();
 }
 
 // Functions create_entities and destroy_entities can take several seconds.
@@ -119,21 +132,24 @@ bool create_entities()
       "chatter2"));
   msg_in.data = micro_ros_string_utilities_init_with_size(500);
 
-  // create timer,
-  RCCHECK(rclc_timer_init_default2(
-      &timer,
-      &support,
-      RCL_MS_TO_NS(500),
-      timer_callback,
-      true));
+  // // create timer,
+  // RCCHECK(rclc_timer_init_default2(
+  //     &timer,
+  //     &support,
+  //     RCL_MS_TO_NS(500),
+  //     timer_callback,
+  //     true));
 
   // create executor
   executor = rclc_executor_get_zero_initialized_executor();
   RCCHECK(rclc_executor_init(&executor, &support.context, 5, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // RCCHECK(rclc_executor_add_timer(&executor, &timer));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg_in, &subscription_callback, ON_NEW_DATA));
 
-  // RCCHECK(rmw_uros_sync_session(5000));
+  if (RMW_RET_OK != rmw_uros_sync_session(2000))
+  {
+    Serial.printf("Failed to sync session");
+  }
 
   return true;
 }
@@ -145,10 +161,13 @@ void destroy_entities()
 
   RCSOFTCHECK(rcl_subscription_fini(&subscriber, &node));
   RCSOFTCHECK(rcl_publisher_fini(&publisher, &node));
-  RCSOFTCHECK(rcl_timer_fini(&timer));
+  // RCSOFTCHECK(rcl_timer_fini(&timer));
   RCSOFTCHECK(rclc_executor_fini(&executor));
   RCSOFTCHECK(rcl_node_fini(&node));
+  RCSOFTCHECK(rcl_init_options_fini(&init_options));
   RCSOFTCHECK(rclc_support_fini(&support));
+
+  micro_ros_string_utilities_destroy(&(msg_in.data));
 }
 
 void setup()
@@ -163,51 +182,63 @@ void setup()
 
   delay(2000);
 
-  state = WAITING_AGENT;
-
   Serial.printf("done, sys time = %d\r\n", rmw_uros_epoch_millis());
 
   create_entities();
+  xLastHeartbeatTime = xTaskGetTickCount();
+
+  xTaskCreate(publisherTask, "publisherTask", 4096, NULL, 1, NULL);
 }
 
-int start = 0;
-rmw_ret_t ret;
+void publisherTask(void *pvParameters)
+{
+  static TickType_t xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    if (pdFALSE == xTaskDelayUntil(&xLastWakeTime, 500))
+    {
+      Serial.printf("publisher loop delayed: %d\r\n", xTaskGetTickCount() - xLastWakeTime);
+      xLastWakeTime = xTaskGetTickCount();
+    }
+    publishHelloWorld();
+  }
+}
 
 void loop()
 {
   static TickType_t xLastWakeTime = xTaskGetTickCount();
-  vTaskDelayUntil(&xLastWakeTime, 10);
-  ret = rclc_executor_spin_some(&executor, 0);
-}
+  static int cnt = 0;
 
-// void loop() {
-//   static TickType_t xLastWakeTime = xTaskGetTickCount();
-//   static int cnt = 0;
-//   vTaskDelayUntil(&xLastWakeTime, 10);
-//   switch (state) {
-//     case WAITING_AGENT:
-//       EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(400, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
-//       break;
-//     case AGENT_AVAILABLE:
-//       state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
-//       if (state == WAITING_AGENT) {
-//         destroy_entities();
-//       };
-//       break;
-//     case AGENT_CONNECTED:
-//       EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(400, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
-//       if (state == AGENT_CONNECTED) {
-//         // rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-//         rclc_executor_spin_some(&executor, 0);
-//       }
-//       break;
-//     case AGENT_DISCONNECTED:
-//       destroy_entities();
-//       state = WAITING_AGENT;
-//       break;
-//     default:
-//       break;
-//   }
+  if (pdFALSE == xTaskDelayUntil(&xLastWakeTime, 10))
+  {
+    Serial.printf("loop delayed: %d\r\n", xTaskGetTickCount() - xLastWakeTime);
+    xLastWakeTime = xTaskGetTickCount();
+  }
+
+  Serial.printf("hb: %d, last: %d, diff: %d\r\n", xLastHeartbeatTime, xLastWakeTime, xLastWakeTime - xLastHeartbeatTime);
+
+  if (xLastWakeTime - xLastHeartbeatTime > pdMS_TO_TICKS(5000))
+  {
+    Serial.printf("No heartbeat received for 5s, restarting...");
+    destroy_entities();
+    while (false == create_entities())
+    {
+      destroy_entities();
+      vTaskDelay(2000);
+      Serial.printf("Failed to create entities, retrying...");
+    }
+    Serial.printf("Reconnection success, sys time = %d\r\n", rmw_uros_epoch_millis());
+
+    if (RMW_RET_OK != rmw_uros_sync_session(2000))
+    {
+      Serial.printf("Failed to sync session");
+    }
+
+    xLastHeartbeatTime = xTaskGetTickCount();
+  }
+
+  rclc_executor_spin_some(&executor, 0);
+};
 
 //   if (state == AGENT_CONNECTED) {
 //     digitalWrite(LED_PIN, 1);
@@ -215,4 +246,3 @@ void loop()
 //     digitalWrite(LED_PIN, 0);
 //   }
 // }
-
